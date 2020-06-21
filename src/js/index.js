116292bcdfe3b202369b78e2d0483dd9648d3f2b
alejoalejo43 <alejandrocastillo.v@hotmail.com>
2020-06-21 17:23:12 -0500

    Building the search controller

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,40 @@
-// Global app controller
-import num from './test';
-const x = 23;
-console.log(`I imported ${num} from another module Variable x is ${x}`);
+/* import str from './models/Search';
+//import { add as a, multiply as m, ID } from './views/searchView';
+console.log(`Using imported functions! ${a(ID, 2)} and ${m(4, 5)}. ${str}`);
+import * as searchView from './views/searchView'
+console.log(`Using imported functions! ${searchView.add(searchView.ID, 2)} and ${searchView.multiply(4, 5)}. ${str}`); */
+
+import Search from './models/Search';
+
+/**Global state of the app
+ * - Search onject
+ * - Current recipe object
+ * - Shopping list object
+ * - Liked recipes
+ */
+const state = {};
+
+const controlSearch = async () => {
+    // 1) Get query from view
+    const query = 'pizza'; //TODO
+
+    if (query) {
+        // 2) New search object and add t state
+        state.search = new Search(query);
+
+        // 3) Prepare UI for results
+
+        // 4) Search for recipes
+        await state.search.getResults();
+
+        // 5) Render results on UI
+        console.log(state.search.result);
+    }
+}
+
+document.querySelector('.search').addEventListener('submit', e => {
+    e.preventDefault();
+    controlSearch();
+})
+
+
